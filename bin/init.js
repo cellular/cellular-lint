b159b274939079e1d93c4065950d04e759f85c38
Felix Gnass <fgnass@gmail.com>
2018-02-12 19:37:40 +0100

    debug

--- bin/init.js
+++ bin/init.js
@@ -15,6 +15,7 @@ const builtInRc = path.resolve(__dirname, '../.eslintrc');
 
 if (hasConfig) {
   console.log(`${projectRc} already exists. Skipping.`);
+  console.log(process.env);
 } else {
   fs.copySync(builtInRc, projectRc);
   console.log(`${projectRc} created.`);
