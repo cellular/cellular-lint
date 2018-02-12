fedabd41acf59fe3e6f9b684a814190d698eaad3
Felix Gnass <fgnass@gmail.com>
2018-02-12 20:47:20 +0100

    fix hasConfig check

--- bin/init.js
+++ bin/init.js
@@ -6,15 +6,13 @@ const path = require('path');
 const { hasPkgProp, hasFile, root } = require('about-this-app');
 
 const hasConfig =
-  hasFile('.eslintrc') ||
-  hasFile('.eslintrc.js') ||
-  !hasPkgProp('eslintConfig');
+  hasFile('.eslintrc') || hasFile('.eslintrc.js') || hasPkgProp('eslintConfig');
 
 const projectRc = path.resolve(root, '.eslintrc');
 const builtInRc = path.resolve(__dirname, '../.eslintrc');
 
 if (hasConfig) {
-  console.log(`${projectRc} already exists. Skipping.`);
+  console.log('Project already has an eslint config.');
 } else {
   fs.copySync(builtInRc, projectRc);
   console.log(`${projectRc} created.`);
