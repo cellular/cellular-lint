b580efaa9ce657cb2b11e991bc50f369bf5fb5d7
Felix Gnass <fgnass@gmail.com>
2018-02-12 19:25:42 +0100

    use fs-extra

---
+++ bin/init.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+// @flow
+
+const fs = require('fs-extra');
+const path = require('path');
+const { hasPkgProp, hasFile, root } = require('about-this-app');
+
+const hasConfig =
+  hasFile('.eslintrc') ||
+  hasFile('.eslintrc.js') ||
+  !hasPkgProp('eslintConfig');
+
+const projectRc = path.resolve(root, '.eslintrc');
+const builtInRc = path.resolve(__dirname, '../.eslintrc');
+
+if (hasConfig) {
+  console.log(`${projectRc} already exists. Skipping.`);
+} else {
+  fs.copySync(builtInRc, projectRc);
+  console.log(`${projectRc} created.`);
+}
